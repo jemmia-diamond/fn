0d4648231bb6f819e238c4a96321260ed908726c
Phong Phan <fongfan999@gmail.com>
2025-05-22 13:56:54 +0700

    chore: test sentry sourcemaps

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ const app = new Hono();
 
 app.use("*", sentry());
 app.get("/sentry", (_c) => {
-  throw new Error("Sentry test");
+  randomError(0.33, "0.33 error");
+  randomError(0.5, "0.5 error");
 });
+
+// Write a function that return error randomly
+function randomError(rate, message) {
+  if (Math.random() < rate) {
+    throw new Error(message);
+  }
+}
 
 export default app;
