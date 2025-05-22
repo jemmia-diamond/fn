6edec833a45feaf8cdf8f01bd3a280a654fab856
Phong Phan <fongfan999@gmail.com>
2025-05-22 13:34:25 +0700

    chore: put back sentry endpoint

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import { sentry } from "@hono/sentry";
 const app = new Hono();
 
 app.use("*", sentry());
+app.get("/sentry", (_c) => {
+  throw new Error("Sentry test");
+});
 
 export default app;
