6a5f225a2d5edfe5633bfa331926c28c2e66e8e4
Phong Phan <fongfan999@gmail.com>
2025-05-22 14:04:01 +0700

    chore: test more sentry sourcemaps

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import { sentry } from "@hono/sentry";
 const app = new Hono();
 
 app.use("*", sentry());
+app.get("/xxx", (_c) => {
+  return c.text("Hello Hono!");
+});
+app.get("/yyy", (_c) => {
+  return c.text("Hello Hono!");
+});
+app
+app.get("/zzz", (_c) => {
+  return c.text("Hello Hono!");
+});
 app.get("/sentry", (_c) => {
   randomError(0.33, "0.33 error");
   randomError(0.5, "0.5 error");
