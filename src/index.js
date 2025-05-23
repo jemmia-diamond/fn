38e4faf3028f9a47ceb0bbb8940269011abda902
Phong Phan <fongfan999@gmail.com>
2025-05-23 10:20:49 +0700

    chore: remove sentry release version

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import { sentry } from "@hono/sentry";
 const app = new Hono();
 
 // Add release and dist information for sourcemap matching
-app.use("*", sentry({
-  release: "1.0.0", // Fixed version instead of process.env
-  dist: "1" // Fixed version instead of process.env
-}));
+app.use("*", sentry());
 app.get("/xxx", (_c) => {
   return c.text("Hello Hono!");
 });
