dfddfbdd17bf2043efa1b13bdd5a24c72f3c6b49
Hung Tran <hungtranthanh0302@gmail.com>
2025-05-28 12:17:20 +0700

    format code

--- src/frappe/utils/datetime.js
+++ src/frappe/utils/datetime.js
@@ -11,20 +11,20 @@ dayjs.extend(utc);
  */
 
 export function formatDate(isoString, type) {
-    /** @type {FormatType} */
-    type = type;
-    if (!isoString || typeof isoString !== "string") return null;
-    // Remove unnecessary spaces
-    const cleaned = isoString.replace(/\s+/g, "");
-    const date = dayjs(cleaned);
-    if (!date.isValid()) {
-        console.error("Invalid date:", isoString);
-        return null;
-    }
-    if (type === "date") {
-        return date.utc().format("YYYY-MM-DD");
-    }
-    // Return datetime with microseconds
-    const milliseconds = String(date.millisecond()).padStart(3, "0") + "000"; // Simulate microseconds
-    return `${date.utc().format("YYYY-MM-DD HH:mm:ss")}.${milliseconds}`;
+  /** @type {FormatType} */
+  type = type;
+  if (!isoString || typeof isoString !== "string") return null;
+  // Remove unnecessary spaces
+  const cleaned = isoString.replace(/\s+/g, "");
+  const date = dayjs(cleaned);
+  if (!date.isValid()) {
+    console.error("Invalid date:", isoString);
+    return null;
+  }
+  if (type === "date") {
+    return date.utc().format("YYYY-MM-DD");
+  }
+  // Return datetime with microseconds
+  const milliseconds = String(date.millisecond()).padStart(3, "0") + "000"; // Simulate microseconds
+  return `${date.utc().format("YYYY-MM-DD HH:mm:ss")}.${milliseconds}`;
 };
