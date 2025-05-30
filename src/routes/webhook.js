0778f33ca35c32e9f7e3ae5656c30878c0b7ac28
Hung Tran <hungtranthanh0302@gmail.com>
2025-05-30 14:42:25 +0700

    fix: import Webhook in webhook route

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -1,5 +1,5 @@
 // TODO: Make this import dynamic, so that we can add new controllers without repeating ourselves.
-import Webhook from "../controllers/webhooks";
+import Webhook from "../controllers/webhook";
 
 export default class WebhookRoutes {
   static register(webhook) {
