598ef3a550a2ab020e5abfffb6fb2ac9cd0d6ec3
Hung Tran <hungtranthanh0302@gmail.com>
2025-06-05 15:52:50 +0700

    chore: lint fix

--- src/services/larksuite/lark.js
+++ src/services/larksuite/lark.js
@@ -1,13 +1,13 @@
-import * as lark from '@larksuiteoapi/node-sdk';
+import * as lark from "@larksuiteoapi/node-sdk";
 import fetchAdapter from "@vespaiach/axios-fetch-adapter";
 
 export default class LarksuiteService {
-    static createClient(env) {
-        const client = new lark.Client({
-            appId: env.LARKSUITE_APP_ID,
-            appSecret: env.LARKSUITE_APP_SECRET,
-        });
-        client.httpInstance.defaults.adapter = fetchAdapter;
-        return client;
-    }
+  static createClient(env) {
+    const client = new lark.Client({
+      appId: env.LARKSUITE_APP_ID,
+      appSecret: env.LARKSUITE_APP_SECRET
+    });
+    client.httpInstance.defaults.adapter = fetchAdapter;
+    return client;
+  }
 };
