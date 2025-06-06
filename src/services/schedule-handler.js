163acabc1731a4ac5b03557b3a91c85e845d53b4
Hung Tran <hungtranthanh0302@gmail.com>
2025-06-06 10:07:07 +0700

    chore: modify cron time

--- src/services/schedule-handler.js
+++ src/services/schedule-handler.js
@@ -3,10 +3,10 @@ import Larksuite from "../services/larksuite";
 export default {
   scheduled: async (controller, env, _ctx) => {
     switch (controller.cron) {
-    case "0 0 * * *":
+    case "0 17 * * *":
       await Larksuite.Contact.UserService.syncUsersToDatabase(env);
       break;
-    case "30 8 * * *":
+    case "30 1 * * *":
       await Larksuite.Attendance.ScheduleService.syncScheduleToDatabase(env);
       break;
     default:
