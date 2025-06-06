2eddcf3c3a2429214ff92e20903752460ffa1a6b
Hung Tran <hungtranthanh0302@gmail.com>
2025-06-06 09:59:10 +0700

    chore: move schedule-handler

--- src/services/schedule-handler.js
+++ src/services/schedule-handler.js
@@ -1,16 +1,8 @@
 import Larksuite from "../services/larksuite";
 
-function scheduledJob(env, ctx) {
-  // Do something 
-  return { env, ctx };
-};
-
 export default {
-  scheduled: async (controller, env, ctx) => {
+  scheduled: async (controller, env, _ctx) => {
     switch (controller.cron) {
-    case "* * * * *":
-      scheduledJob(env, ctx);
-      break;
     case "0 0 * * *":
       await Larksuite.Contact.UserService.syncUsersToDatabase(env);
       break;
