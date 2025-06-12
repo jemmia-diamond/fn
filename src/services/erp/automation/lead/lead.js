ccfabea830a894e71a63ea95921f5d863a99b191
khoadn <khoa.nguyendangse@gmail.com>
2025-06-12 14:38:39 +0700

    fix: conflict

--- src/services/erp/automation/lead/lead.js
+++ src/services/erp/automation/lead/lead.js
@@ -1,9 +1,7 @@
-
 import LeadService from "../../crm/lead/lead";
 export class AutomationLeadService {
-  static async handleSyncLeadFromPancake(env){
+  static async handleSyncLeadFromPancake(env) {
     let leadService = new LeadService(env);
     await leadService.handleSyncLead();
   }
-  
 }
