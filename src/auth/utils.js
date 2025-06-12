6d96b808859e15f1b5af3fcca288cd3adcfd571a
Hung Tran <hungtranthanh0302@gmail.com>
2025-06-12 10:26:14 +0700

    lint format

--- src/auth/utils.js
+++ src/auth/utils.js
@@ -2,6 +2,6 @@ import { HmacSHA256 } from "crypto-js";
 import Base64 from "crypto-js/enc-base64";
 
 export const generateHmacBase64 = (plainText, secret) => {
-    const computedHmac = Base64.stringify(HmacSHA256(plainText, secret));
-    return computedHmac;
-}
+  const computedHmac = Base64.stringify(HmacSHA256(plainText, secret));
+  return computedHmac;
+};
