b8889142a4a9278c5970f327235a0caf2a9ddd41
Hung Tran <hungtranthanh0302@gmail.com>
2025-06-12 10:20:03 +0700

    Update src/controllers/pancake/conversation-assignment.js
    Co-authored-by: Phong Phan <fongfan999@gmail.com>

--- src/controllers/pancake/conversation-assignment.js
+++ src/controllers/pancake/conversation-assignment.js
@@ -5,7 +5,7 @@ export default class ConversationAssignmentController {
     const data = await ctx.req.json();
     const conversationAssignmentService = new Pancake.ConversationAssignmentService(ctx.env);
     const result = await conversationAssignmentService.syncConversationAssigneesWithERPToDo(data);
-    if (!result) return ctx.json({ success: false });
-    return ctx.json({ success: true });
+    
+    return ctx.json({ success: !!result });
   }
 }
