e146e65a9e0981e70f2fa97f54de90a7b9d39b18
Hung Tran <hungtranthanh0302@gmail.com>
2025-06-12 10:31:44 +0700

    enhance: add reusable generateHmacBase64 function

--- src/auth/frappe-auth.js
+++ src/auth/frappe-auth.js
@@ -1,6 +1,5 @@
 import { HTTPException } from "hono/http-exception";
-import { HmacSHA256 } from "crypto-js";
-import Base64 from "crypto-js/enc-base64";
+import { generateHmacBase64 } from "./utils";
 
 const verifyFrappeWebhook = async (ctx, next) => {
   const signature = ctx.req.header("X-Frappe-Webhook-Signature");
@@ -9,7 +8,7 @@ const verifyFrappeWebhook = async (ctx, next) => {
   }
   const rawBody = await ctx.req.text();
   const secret = ctx.env.FRAPPE_WEBHOOK_SECRET;
-  const computedHmac = Base64.stringify(HmacSHA256(rawBody, secret));
+  const computedHmac = generateHmacBase64(rawBody, secret);
   if (signature !== computedHmac) {
     throw new HTTPException(401, "Unauthorized");
   }
