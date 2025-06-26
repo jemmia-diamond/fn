f5fde6cecdb64560646d2e96657da3a379257d07
Hung Tran <hungtranthanh0302@gmail.com>
2025-06-26 15:31:37 +0700

    fix: variable name constraint

--- src/auth/pancake-auth.js
+++ src/auth/pancake-auth.js
@@ -2,9 +2,9 @@ import { HTTPException } from "hono/http-exception";
 
 export const verifyPancakeWebhook = async (ctx, next) => {
   const headers = ctx.req.headers;
-  const PANCAKE_ALLOWED_IPS = ctx.env.PANCAKE_ALLOWED_IPS ? ctx.env.PANCAKE_ALLOWED_IPS.split(",") : ["203.171.22.6"];
-  const X_REAL_IP = headers["x-real-ip"];
-  if (!PANCAKE_ALLOWED_IPS.includes(X_REAL_IP)) {
+  const pancakeAllowedIPs = ctx.env.PANCAKE_ALLOWED_IPS.split(",");
+  const xRealIP = headers["x-real-ip"];
+  if (!pancakeAllowedIPs.includes(xRealIP)) {
     throw new HTTPException(401, "Unauthorized");
   }
   await next();
