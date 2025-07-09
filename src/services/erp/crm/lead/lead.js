58d7995fc9abc8f8130b913ab77f4ba3ba544778
Hung Tran <hungtranthanh0302@gmail.com>
2025-07-09 12:16:42 +0700

    change sync logic

--- src/services/erp/crm/lead/lead.js
+++ src/services/erp/crm/lead/lead.js
@@ -169,7 +169,7 @@ export default class LeadService {
 
   static async syncWebsiteLeads(env) {
     const leadService = new LeadService(env);
-    const timeThreshold = dayjs().utc().subtract(10, "hour").subtract(5, "minutes").format("YYYY-MM-DD HH:mm:ss");
+    const timeThreshold = dayjs().utc().subtract(1, "hour").subtract(5, "minutes").format("YYYY-MM-DD HH:mm:ss");
     const leads = await leadService.getWebsiteLeads(timeThreshold);
     if (leads.length) {
       for (const lead of leads) {
