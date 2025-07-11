94a978b46c5b4f4e4ebe146784bdc74962de773e
Hung Tran <hungtranthanh0302@gmail.com>
2025-07-11 12:03:28 +0700

    enhance: use constant for phone config

--- src/stringee/utils/phone-number.js
+++ src/stringee/utils/phone-number.js
@@ -1,6 +1,10 @@
+const PHONE_NUMBER_LENGTH_WITH_COUNTRY_CODE = 11;
+const VIETNAM_COUNTRY_CODE = "84";
+const VIETNAM_PHONE_PREFIX = "0";
+
 export const normalizePhoneNumber = (phone) => {
-  if (phone.length === 11 && phone.startsWith("84")) {
-    return "0" + phone.slice(2);
+  if (phone.length === PHONE_NUMBER_LENGTH_WITH_COUNTRY_CODE && phone.startsWith(VIETNAM_COUNTRY_CODE)) {
+    return VIETNAM_PHONE_PREFIX + phone.slice(2);
   }
   return phone;
 };
