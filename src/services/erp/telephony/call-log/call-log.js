38171879ee769070ecf3c079da71ce3334536964
Hung Tran <hungtranthanh0302@gmail.com>
2025-07-11 10:46:08 +0700

    feature: sync stringee call-log to erp

--- src/services/erp/telephony/call-log/call-log.js
+++ src/services/erp/telephony/call-log/call-log.js
@@ -1,6 +1,11 @@
-import FrappeClient from "../../../../frappe/frappe-client";
-import StringeeClient from "../../../../stringee/stringee-client";
-import { timestampToDatetime } from "../../../../stringee/utils/datetime"
+import FrappeClient from "src/frappe/frappe-client";
+import StringeeClient from "src/stringee/stringee-client";
+import { timestampToDatetime } from "src/stringee/utils/datetime";
+import { normalizePhoneNumber } from "src/stringee/utils/phone-number";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc.js";
+
+dayjs.extend(utc);
 
 export default class CallLogService {
   constructor(env) {
@@ -11,14 +16,16 @@ export default class CallLogService {
   }
 
   static async syncStringeeCallLogs(env) {
+    const currentTimestamp = dayjs.utc().subtract(1, "hour").subtract(5, "minutes").unix();
     const callLogService = new CallLogService(env);
     const callLogs = await callLogService.stringeeClient.getCallLogs({
-      page: 2,
+      page: 1,
+      limit: 100,
+      from_created_time: currentTimestamp
     });
     for (const callLog of callLogs) {
       const mappedCallLog = callLogService.mapStringeeCallLogFields(callLog);
       await callLogService.frappeClient.upsert(mappedCallLog, "id");
-      console.log(callLog);
     }
   }
 
@@ -29,8 +36,8 @@ export default class CallLogService {
     return {
       doctype: this.doctype,
       id: callLog.id,
-      from: callLog.from_number,
-      to: callLog.to_number,
+      from: normalizePhoneNumber(callLog.from_number),
+      to: normalizePhoneNumber(callLog.to_number),
       start_time: timestampToDatetime(callLog.start_time),
       end_time: timestampToDatetime(callLog.stop_time),
       duration: callLog.answer_duration,
---
+++ src/stringee/utils/phone-number.js
@@ -0,0 +1,6 @@
+export const normalizePhoneNumber = (phone) => {
+  if (phone.length === 11 && phone.startsWith("84")) {
+    return "0" + phone.slice(2);
+  }
+  return phone;
+}
