7b9fe7aaf10d3d74a72cf7e9602a42bb22052bed
Hung Tran <hungtranthanh0302@gmail.com>
2025-07-14 09:31:37 +0700

    fix: modify service constructor

--- src/services/erp/telephony/call-log/call-log.js
+++ src/services/erp/telephony/call-log/call-log.js
@@ -4,25 +4,42 @@ import { timestampToDatetime } from "src/stringee/utils/datetime";
 import { normalizePhoneNumber } from "src/stringee/utils/phone-number";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc.js";
-import { customizeEnv } from "services/env-handler";
 
 dayjs.extend(utc);
 
 export default class CallLogService {
-  constructor(env) {
+  constructor(
+    {
+      jemmiaErpBaseUrl,
+      jemmiaErpApiKey,
+      jemmiaErpApiSecret,
+      stringeeApiKeySid,
+      stringeeApiKeySecret
+    }
+  ) {
     this.doctype = "Call Log";
-    this.env = env;
-    this.frappeClient = new FrappeClient({ url: env.JEMMIA_ERP_BASE_URL, apiKey: env.JEMMIA_ERP_API_KEY, apiSecret: env.JEMMIA_ERP_API_SECRET });
-    this.stringeeClient = new StringeeClient(
-      env.STRINGEE_SID_SECRET || env.STRINGEE_API_KEY_SID, 
-      env.STRINGEE_KEY_SECRET || env.STRINGEE_API_KEY_SECRET);
+    this.frappeClient = new FrappeClient({ url: jemmiaErpBaseUrl, apiKey: jemmiaErpApiKey, apiSecret: jemmiaErpApiSecret });
+    this.stringeeClient = new StringeeClient(stringeeApiKeySid, stringeeApiKeySecret);
     this.stringeeRecordingPrefix = `${this.stringeeClient.baseUrl}/call/recording`;
   }
 
   static async syncStringeeCallLogs(env) {
-    env = await customizeEnv(env);
+    const JEMMIA_ERP_BASE_URL = env.JEMMIA_ERP_BASE_URL;
+    const JEMMIA_ERP_API_KEY = env.JEMMIA_ERP_API_KEY;
+    const JEMMIA_ERP_API_SECRET = env.JEMMIA_ERP_API_SECRET;
+    const STRINGEE_API_KEY_SID = env.STRINGEE_SID_SECRET.get();
+    const STRINGEE_API_KEY_SECRET = env.STRINGEE_KEY_SECRET.get();
+
     const currentTimestamp = dayjs.utc().subtract(1, "hour").subtract(5, "minutes").unix();
-    const callLogService = new CallLogService(env);
+    const callLogService = new CallLogService(
+      {
+        jemmiaErpBaseUrl: JEMMIA_ERP_BASE_URL,
+        jemmiaErpApiKey: JEMMIA_ERP_API_KEY,
+        jemmiaErpApiSecret: JEMMIA_ERP_API_SECRET,
+        stringeeApiKeySid: STRINGEE_API_KEY_SID,
+        stringeeApiKeySecret: STRINGEE_API_KEY_SECRET
+      }
+    );
     const callLogs = await callLogService.stringeeClient.getCallLogs({
       page: 1,
       limit: 100,
