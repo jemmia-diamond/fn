50bd6e65be59da56cc785b84ecf1958acabd1d16
Hung Tran <hungtranthanh0302@gmail.com>
2025-07-28 23:35:28 +0700

    remove underscore of ctx

--- src/controllers/webhook/inventory-cms/inventory-check-sheet.js
+++ src/controllers/webhook/inventory-cms/inventory-check-sheet.js
@@ -1,5 +1,5 @@
 export default class InventoryCheckSheetController {
-  static async create(_ctx) {
-    return _ctx.json({ message: "Inventory check sheet received" });
+  static async create(ctx) {
+    return ctx.json({ message: "Inventory check sheet received" });
   }
 }
