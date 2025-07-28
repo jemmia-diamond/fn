591a858e18ba5b13ec565421efa4e40185343e0b
Hung Tran <hungtranthanh0302@gmail.com>
2025-07-28 09:54:23 +0700

    enhance: add pages to query params

--- src/controllers/ecommerce/product-controller.js
+++ src/controllers/ecommerce/product-controller.js
@@ -9,6 +9,7 @@ export default class ProductController {
       product_types: params.product_types ? params.product_types.split(",") : [],
       material_colors: params.material_colors ? params.material_colors.split(",") : [],
       genders: params.gender ? params.gender.split(",") : [],
+      pages: params.pages ? params.pages.split(",") : [],
       pagination: {
         from: params.from ? parseInt(params.from, 10) : 1,
         limit: params.limit ? parseInt(params.limit, 10) : 24
@@ -24,4 +25,3 @@ export default class ProductController {
     return ctx.json(result);
   }
 }
-
