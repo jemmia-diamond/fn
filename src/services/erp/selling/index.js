596a72264b2c680a0ee7a94faf231eabb592b34b
Hung Tran <hungtranthanh0302@gmail.com>
2025-08-04 14:30:30 +0700

    fix: typo

--- src/services/erp/selling/index.js
+++ src/services/erp/selling/index.js
@@ -1,6 +1,6 @@
 import SalesOrderService from "src/services/erp/selling/sales-order/sales-order";
 import CustomerService from "src/services/erp/selling/customer/customer";
-import SalesPersonService from "services/erp/selling/sales-person/sales-persion";
+import SalesPersonService from "services/erp/selling/sales-person/sales-person";
 
 export default {
   SalesOrderService: SalesOrderService,
