83fc3e07485c34e098997d9fae1bada75d256332
Phong Phan <fongfan999@gmail.com>
2025-08-07 13:57:17 +0700

    chore: add eslint to prevent comments in Vietnamese

---
+++ eslint/rules/no-vietnamese-text.js
@@ -0,0 +1,41 @@
+class NoVietnameseTextValidator {
+  static get meta() {
+    return {
+      type: "problem",
+      docs: {
+        description: "Disallow Vietnamese text in comments, only English is allowed"
+      },
+      fixable: null,
+      schema: []
+    };
+  }
+
+  static create(context) {
+    const vietnamesePattern = /[àáạảãâầấậẩẫăằắặẳẵèéẹẻẽêềếệểễìíịỉĩòóọỏõôồốộổỗơờớợởỡùúụủũưừứựửữỳýỵỷỹđĐÀÁẠẢÃÂẦẤẬẨẪĂẰẮẶẲẴÈÉẸẺẼÊỀẾỆỂỄÌÍỊỈĨÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠÙÚỤỦŨƯỪỨỰỬỮỲÝỴỶỸ]/;
+
+    function checkText(node, text) {
+      if (text && vietnamesePattern.test(text)) {
+        context.report({
+          node,
+          message: "Vietnamese text in comments is not allowed. Only English comments are permitted."
+        });
+      }
+    }
+
+    return {
+      Program() {
+        const sourceCode = context.getSourceCode();
+        const comments = sourceCode.getAllComments();
+        
+        comments.forEach(comment => {
+          checkText(comment, comment.value);
+        });
+      }
+    };
+  }
+}
+
+export default {
+  meta: NoVietnameseTextValidator.meta,
+  create: NoVietnameseTextValidator.create
+};
