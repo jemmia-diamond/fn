dea9cd3010f0fa1e58b07337ce76f6b022b453d6
Hoàng Hưng <hoanghung@MacBook-Pro-cua-Hoang.local>
2025-08-08 16:03:53 +0700

    add redirect

--- src/controllers/redirect-controller.js
+++ src/controllers/redirect-controller.js
@@ -2,7 +2,7 @@ export default class RedirectController {
   static MAPPINGS = {
     "warranty-policy": "https://jemmia.vn/pages/chinh-sach-bao-hanh-va-thu-mua-thu-doi-san-pham#:~:text=III.%20CH%C3%8DNH%20S%C3%81CH%20B%E1%BA%A2O%20H%C3%80NH",
     "return-and-exchange-policy": "https://jemmia.vn/pages/chinh-sach-bao-hanh-va-thu-mua-thu-doi-san-pham#:~:text=IV.%20CH%C3%8DNH%20S%C3%81CH%20THU%20MUA%20%E2%80%93%20THU%20%C4%90%E1%BB%94I",
-    "order-tracking": "https://jemmia.vn/pages/chinh-sach-bao-mat"
+    "order-tracking": "https://jemmia.vn/pages/tra-cuu-don-hang?order_id=123"
   };
 
   static async show(ctx) {    
@@ -10,7 +10,7 @@ export default class RedirectController {
 
     const url = RedirectController.MAPPINGS[name];
     if (!url) {
-      ctx.res.status(404);
+      ctx.status(404);
       return;
     }
 
