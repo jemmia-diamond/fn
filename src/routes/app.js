036bf20d9eb34c98c9522f36169fed6e9822e696
Phong Phan <fongfan999@gmail.com>
2025-08-08 15:28:44 +0700

    chore: use get over use

--- src/routes/app.js
+++ src/routes/app.js
@@ -2,6 +2,6 @@ import RedirectController from "controllers/redirect-controller";
 
 export default class AppRoutes {
   static register(app) {
-    app.use("/redirects/:name", RedirectController.show);
+    app.get("/redirects/:name", RedirectController.show);
   };
 };
