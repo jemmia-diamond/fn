2c7b2283276a7121da3907e20cd08373a27ac7f3
TungLam111 <lamtungphan8@gmail.com>
2025-08-13 15:55:19 +0700

    fix review

--- src/controllers/webhook/haravan/index.js
+++ src/controllers/webhook/haravan/index.js
@@ -1,6 +1,5 @@
 import { verifyHmacBase64Auth } from "auth/hmac-base64-auth";
 import HaravanERPOrderController from "controllers/webhook/haravan/erp/order";
-import HaravanLarkTempProductOrderController from "controllers/webhook/haravan/lark/temp_product_order";
 
 export default class HaravanWebhook {
   static register(webhook) {
@@ -11,6 +10,5 @@ export default class HaravanWebhook {
     
     haravanWebhookNamespace.use("*", verifyHmacBase64Auth("X-Haravan-Hmacsha256", "HARAVAN_WEBHOOK_SECRET"));
     haravanWebhookNamespace.post("erp/orders", HaravanERPOrderController.create);
-    haravanWebhookNamespace.post("lark/temp_product_order", HaravanLarkTempProductOrderController.create);
   };
 };
