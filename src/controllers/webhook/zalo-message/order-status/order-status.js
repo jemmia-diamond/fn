f7ea5be65d32365fb1685c450763ebb3dfb4ed0c
Hoàng Hưng <hoanghung@MacBook-Pro-cua-Hoang.local>
2025-08-15 16:25:51 +0700

    add header validation

--- src/controllers/webhook/zalo-message/order-status/order-status.js
+++ src/controllers/webhook/zalo-message/order-status/order-status.js
@@ -2,6 +2,9 @@ export default class OrderStatusController {
   static async create(ctx) {
     const data = await ctx.req.json();
     const templateId = await ctx.req.header("template_id");
+    if (!templateId) {
+      return ctx.json({ message: "Template ID is required", status: 400 });
+    }
     data.template_id = Number(templateId);
     try {
       await ctx.env["ZALO_MESSAGE_QUEUE"].send(data);
