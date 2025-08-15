9471f494c0011dd9a7bff4c69645d07ad1ba5e79
TungLam111 <lamtungphan8@gmail.com>
2025-08-15 10:14:24 +0700

    add endpoint for nhattin tracking

--- src/routes/api.js
+++ src/routes/api.js
@@ -41,6 +41,7 @@ export default class APIRoutes {
     ecommerceNamespaceApi.get("/products/jewelries", Ecommerce.JewelryController.index);
     ecommerceNamespaceApi.get("/products/wedding_rings", Ecommerce.WeddingRingController.index);
     ecommerceNamespaceApi.delete("/ind-day-stats", Ecommerce.IndDayStatController.destroy);
+    ecommerceNamespaceApi.get("/order-trackings/nhattin/:id", Ecommerce.NhattinTrackingController.show);
     ecommerceNamespaceApi.get("/order-trackings/:id", Ecommerce.OrderTrackingController.show);
   };
 };
