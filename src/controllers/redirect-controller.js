772a2a4c5a2b945aae7afd29308a7ad56482e106
Hoàng Hưng <hoanghung@MacBook-Pro-cua-Hoang.local>
2025-08-21 16:45:04 +0700

    dfsfds

--- src/controllers/redirect-controller.js
+++ src/controllers/redirect-controller.js
@@ -2,7 +2,7 @@ export default class RedirectController {
   static MAPPINGS = {
     "warranty-policy": "https://jemmia.vn/pages/chinh-sach-bao-hanh-va-thu-mua-thu-doi-san-pham#:~:text=III.%20CH%C3%8DNH%20S%C3%81CH%20B%E1%BA%A2O%20H%C3%80NH",
     "return-and-exchange-policy": "https://jemmia.vn/pages/chinh-sach-bao-hanh-va-thu-mua-thu-doi-san-pham#:~:text=IV.%20CH%C3%8DNH%20S%C3%81CH%20THU%20MUA%20%E2%80%93%20THU%20%C4%90%E1%BB%94I",
-    "order-tracking": "https://jemmia.vn/pages/tra-cuu-don-hang?order_id=123"
+    "order-tracking": "https://jemmia.vn/pages/tra-cuu-don-hang?order_id=1714590904"
   };
 
   static async show(ctx) {    
