b7655be9bc08e06b74a86bfa2439ceba3cd7dd3b
quanghung0808 <duongquanghung0122@gmail.com>
2025-08-23 12:29:28 +0700

    fix: Validate value before sending Zalo message

--- src/services/ecommerce/zalo-message/zalo-message.js
+++ src/services/ecommerce/zalo-message/zalo-message.js
@@ -7,6 +7,7 @@ export default class SendZaloMessage {
     this.env = env;
   }
   static whitelistPhones = ["0862098011", "0829976232"];
+  static whitelistSource = "web";
 
   static async sendZaloMessage(phone, templateId, templateData, env) {
     try {
@@ -18,10 +19,20 @@ export default class SendZaloMessage {
     }
   }
 
+  static eligibleForSendingZaloMessage(message) {
+    if (this.whitelistSource.includes(message?.source)
+        && this.whitelistPhones.includes(message?.billing_address?.phone 
+        && message.ref_order_id === 0)) {
+      return true;
+    }
+
+    return false;
+  }
+
   static async dequeueSendZaloMessageQueue(batch, env) {
     const messages = batch.messages;
     for (const message of messages) {
-      if (!this.whitelistPhones.includes(message.body.billing_address?.phone)) {
+      if (!eligibleForSendingZaloMessage(message.body)) {
         return;
       }
 
