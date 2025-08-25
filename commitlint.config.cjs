3c84b5788fc3c7f52c7765a2419f49359f957753
quanghung0808 <duongquanghung0122@gmail.com>
2025-08-25 14:02:36 +0700

    refactor(commitlint): update config rules to follow Conventional Commits

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,9 +1,30 @@
 const config = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "references-empty": [1, "never"],
-    "footer-max-line-length": [0, "always"],
-    "body-max-line-length": [0, "always"]
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert"
+      ]
+    ],
+    "type-empty": [2, "never"],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "scope-empty": [0],
+    "header-max-length": [2, "always", 100],
+    "body-leading-blank": [1, "always"],
+    "footer-leading-blank": [1, "always"]
   }
 };
 
