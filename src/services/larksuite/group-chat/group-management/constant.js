0262c25bd5d51aa43c4e688bdc360681cafb25dd
Hung Tran <hungtranthanh0302@gmail.com>
2025-08-25 15:22:26 +0700

    group name to be lower

--- src/services/larksuite/group-chat/group-management/constant.js
+++ src/services/larksuite/group-chat/group-management/constant.js
@@ -1,14 +1,14 @@
 export const CHAT_GROUPS = {
   TESTING_GROUP: {
     chat_id: "oc_7f6dd355251aa766220a84dcae2403e1",
-    GROUP_NAME: "Testing Group"
+    group_name: "Testing Group"
   },
   SUPPORT_ERP: {
     chat_id: "oc_d21fac86f4f26dc94676027a6c467e29",
-    GROUP_NAME: "ERP - Hỗ trợ Presales"
+    group_name: "ERP - Hỗ trợ Presales"
   },
   NEGATIVE_STOCK_ORDERING_CONTROLL: {
     chat_id: "oc_ff35722f14a778cc671688d256d652ed",
-    GROUP_NAME: "[BOT] Kiểm soát tồn kho âm"
+    group_name: "[BOT] Kiểm soát tồn kho âm"
   }
 };
