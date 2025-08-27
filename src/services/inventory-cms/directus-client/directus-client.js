c8f8499d2af2e2b7676844dc3df4e7f2f7f014ef
Hung Tran <hungtranthanh0302@gmail.com>
2025-08-27 19:40:13 +0700

    Update directus-client.js

--- src/services/inventory-cms/directus-client/directus-client.js
+++ src/services/inventory-cms/directus-client/directus-client.js
@@ -2,7 +2,7 @@ import { createDirectus, staticToken, rest } from "@directus/sdk";
 
 export default class DirectusClient {
   static async createClient(env) {
-    const enpoint = env.INVENTORY_CMS_ENPOINT;
+    const enpoint = env.INVENTORY_CMS_ENDPOINT;
     const token = await env.INVENTORY_CMS_STATIC_TOKEN_SECRET.get();
     const client = createDirectus(enpoint).with(rest()).with(staticToken(token));
     return client;
