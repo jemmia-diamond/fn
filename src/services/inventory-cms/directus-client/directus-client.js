d31e7b2e269b5a82da13db658dd0e03f8e0999b4
Hung Tran <hungtranthanh0302@gmail.com>
2025-08-28 11:53:13 +0700

    refactor(inventory-cms): seraprate directus and inventory-cms client

--- src/services/inventory-cms/directus-client/directus-client.js
+++ src/services/inventory-cms/directus-client/directus-client.js
@@ -1,9 +1,7 @@
 import { createDirectus, staticToken, rest } from "@directus/sdk";
 
 export default class DirectusClient {
-  static async createClient(env) {
-    const endpoint = env.INVENTORY_CMS_ENDPOINT;
-    const token = await env.INVENTORY_CMS_STATIC_TOKEN_SECRET.get();
+  static async createClient(endpoint, token) {
     const client = createDirectus(endpoint).with(rest()).with(staticToken(token));
     return client;
   }
