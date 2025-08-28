bb643f105c40b9af66e0a0a6d8fdc938df78bb4d
Hung Tran <hungtranthanh0302@gmail.com>
2025-08-28 11:59:54 +0700

    chore(directus-client): remove async

--- src/services/inventory-cms/directus-client/directus-client.js
+++ src/services/inventory-cms/directus-client/directus-client.js
@@ -1,7 +1,7 @@
 import { createDirectus, staticToken, rest } from "@directus/sdk";
 
 export default class DirectusClient {
-  static async createClient(endpoint, token) {
+  static createClient(endpoint, token) {
     const client = createDirectus(endpoint).with(rest()).with(staticToken(token));
     return client;
   }
