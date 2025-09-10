b63f905e3de193ad099a07cfd7cf4f3805d9cffa
Phong Phan <fongfan999@gmail.com>
2025-09-10 10:55:55 +0700

    chore(error-handler): clean up logging

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -10,12 +10,7 @@ export default async (c, next) => {
     }
 
     // Log the error with context
-    console.error("Unhandled error in request:", error, {
-      error: error.message,
-      path: c.req.path,
-      method: c.req.method
-      // Add any other relevant context
-    });
+    console.error("Error:", error);
 
     // Return generic 500 error to client
     throw new HTTPException(500, "Internal server error");
