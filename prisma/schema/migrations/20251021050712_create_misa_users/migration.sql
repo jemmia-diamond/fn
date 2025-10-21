9f37646073c930e52c2cb1fc2b21d146755c4a69
BinDK <sarakenny123@yahoo.com>
2025-10-21 12:49:26 +0700

    feat(misa): remove index on haravan_id and email

--- prisma/schema/migrations/20251021050712_create_misa_users/migration.sql
+++ prisma/schema/migrations/20251021050712_create_misa_users/migration.sql
@@ -15,15 +15,3 @@ CREATE TABLE "misa"."users" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_employee_code_key" ON "misa"."users"("employee_code");
-
--- CreateIndex
-CREATE UNIQUE INDEX "users_haravan_id_key" ON "misa"."users"("haravan_id");
-
--- CreateIndex
-CREATE UNIQUE INDEX "users_email_key" ON "misa"."users"("email");
-
--- CreateIndex
-CREATE INDEX "users_haravan_id_idx" ON "misa"."users"("haravan_id");
-
--- CreateIndex
-CREATE INDEX "users_email_idx" ON "misa"."users"("email");
