e35220303d690cf42f10b4247dcb69c7c85a40c9
TungLam111 <lamtungphan8@gmail.com>
2025-10-21 13:44:57 +0700

    feat(erp): import missing

--- src/controllers/webhook/frappe/index.js
+++ src/controllers/webhook/frappe/index.js
@@ -1,6 +1,7 @@
 import { verifyHmacBase64Auth } from "auth/hmac-base64-auth";
 import FrappePancakeConversationAssignmentController from "controllers/webhook/frappe/pancake/conversation-assignment";
 import FrappeERPContactsController from "controllers/webhook/frappe/erp/contacts/contacts";
+import FrappeERPSalesOrderController from "controllers/webhook/frappe/erp/sales-order/sales-order";
 
 export default class FrappeWebhook {
   static register(webhook) {
