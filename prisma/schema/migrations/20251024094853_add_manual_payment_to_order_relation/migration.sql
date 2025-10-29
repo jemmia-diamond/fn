41a1ed1995515a15e8692159deefa1ed543ab626
BinDK <sarakenny123@yahoo.com>
2025-10-29 10:18:30 +0700

    chore(misa): remove unused migration file

--- prisma/schema/migrations/20251024094853_add_manual_payment_to_order_relation/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE "payment"."manual_payments" ADD CONSTRAINT "manual_payments_haravan_order_id_fkey" FOREIGN KEY ("haravan_order_id") REFERENCES "haravan"."orders"("id") ON DELETE SET NULL ON UPDATE CASCADE;
