4c6e43909ade8dc442aabf4cb0b757ad2722e2b7
BinDK <sarakenny123@yahoo.com>
2025-10-29 11:08:49 +0700

    fix(misa): removed default value for qr_geneartor.misa_synced_at

--- prisma/schema/migrations/20251029012955_add_misa_sync_fields/migration.sql
+++ prisma/schema/migrations/20251029012955_add_misa_sync_fields/migration.sql
@@ -4,6 +4,6 @@ ALTER TABLE "payment"."manual_payments" ADD COLUMN     "misa_synced_at" TIMESTAM
 -- AlterTable
 ALTER TABLE "ecom"."qr_generator"
   ADD COLUMN  "misa_synced" BOOLEAN NOT NULL DEFAULT FALSE,
-  ADD COLUMN  "misa_synced_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+  ADD COLUMN  "misa_synced_at" TIMESTAMP(6),
   ADD COLUMN  "misa_sync_guid" VARCHAR(255),
   ADD COLUMN  "misa_sync_error_msg" TEXT;
