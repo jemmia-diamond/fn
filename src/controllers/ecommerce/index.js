433bd07f6db001ad2d6195c53508069901591c00
TungLam111 <lamtungphan8@gmail.com>
2025-10-31 09:26:41 +0700

    feat(website): schedule to refill diamond for jemmia online

--- src/controllers/ecommerce/index.js
+++ src/controllers/ecommerce/index.js
@@ -7,6 +7,7 @@ import IndDayStatController from "controllers/ecommerce/ind-day-stat-controller"
 import OrderTrackingController from "controllers/ecommerce/order-tracking-controller";
 import CardController from "controllers/ecommerce/card-controller";
 import ProductAvailabilityController from "controllers/ecommerce/product-availability-controller";
+import JewelryDiamondPairController from "controllers/ecommerce/jewelry-diamond-pair-controller";
 
 export default {
   JewelryController: JewelryController,
@@ -17,5 +18,6 @@ export default {
   IndDayStatController: IndDayStatController,
   OrderTrackingController: OrderTrackingController,
   CardController: CardController,
-  ProductAvailabilityController: ProductAvailabilityController
+  ProductAvailabilityController: ProductAvailabilityController,
+  JewelryDiamondPairController: JewelryDiamondPairController
 };
