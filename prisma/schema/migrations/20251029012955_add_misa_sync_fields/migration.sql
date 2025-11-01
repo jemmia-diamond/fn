c1435d5a914a016ddf8f6bf1857f2e8e2d59f8f6
Nhats 💎 <nhatsdevil@gmail.com>
2025-11-01 17:37:47 +0700

    feat(payment): add misa sync fields to payment tables

---
+++ prisma/schema/migrations/20251029012955_add_misa_sync_fields/migration.sql
@@ -0,0 +1,9 @@
+-- AlterTable
+ALTER TABLE "payment"."manual_payments" ADD COLUMN     "misa_synced_at" TIMESTAMP(6);
+
+-- AlterTable
+ALTER TABLE "ecom"."qr_generator"
+  ADD COLUMN  "misa_synced" BOOLEAN NOT NULL DEFAULT FALSE,
+  ADD COLUMN  "misa_synced_at" TIMESTAMP(6),
+  ADD COLUMN  "misa_sync_guid" VARCHAR(255),
+  ADD COLUMN  "misa_sync_error_msg" TEXT;
