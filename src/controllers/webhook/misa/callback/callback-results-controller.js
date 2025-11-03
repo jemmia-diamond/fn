0139c1a380311fbaa5db1aecfdde15b5b3ebc465
BinDK <sarakenny123@yahoo.com>
2025-11-03 15:52:47 +0700

    refactor(misa): correcting one minute delay constant

--- src/controllers/webhook/misa/callback/callback-results-controller.js
+++ src/controllers/webhook/misa/callback/callback-results-controller.js
@@ -1,12 +1,12 @@
 import * as Sentry from "@sentry/cloudflare";
 
-export default class CallbackResultsController {
-  static ONE_MINUTE_DELAY = 60;
+const ONE_MINUTE_DELAY = 60;
 
+export default class CallbackResultsController {
   static async create(ctx) {
     try {
       const payload = await ctx.req.json();
-      await ctx.env["MISA_QUEUE"].send(payload, { delaySeconds: this.ONE_MINUTE_DELAY });
+      await ctx.env["MISA_QUEUE"].send(payload, { delaySeconds: ONE_MINUTE_DELAY });
       return ctx.json({ message: "Message receive", status: 200 });
     } catch (e) {
       Sentry.captureException(e);
