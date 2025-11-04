b539d14420205f21ff3d334fd571173d549619c0
BinDK <sarakenny123@yahoo.com>
2025-11-04 14:20:13 +0700

    Revert "Merge pull request #352 from jemmia-diamond/feature/misa/create-cash-accounting-document-proposal"
    This reverts commit 123655d2604e7122fd4c756d6d97e25276b9b77c, reversing
    changes made to fd61d8f66a998669efd8dd1d1eb311ca6b1f61b3.

--- src/services/payment/index.js
+++ src/services/payment/index.js
@@ -1,9 +1,5 @@
 import ManualPaymentService from "services/payment/manual-pay/payment";
-import QrPaymentFetchingService from "services/payment/qr_payment/fetch-service";
-import ManualPaymentFetchingService from "services/payment/manual-pay/fetch-service";
 
 export default {
-  ManualPaymentService: ManualPaymentService,
-  QrPaymentFetchingService,
-  ManualPaymentFetchingService
+  ManualPaymentService: ManualPaymentService
 };
