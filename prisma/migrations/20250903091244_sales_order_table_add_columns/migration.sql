69f57416c4fd9e32a5767c2a3d717bff18a3555c
Phong Phan <fongfan999@gmail.com>
2025-11-05 22:54:13 +0700

    chore: baselining migartion

--- prisma/migrations/20250903091244_sales_order_table_add_columns/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "erpnext"."sales_orders" ADD COLUMN     "consultation_date" DATE,
-ADD COLUMN     "primary_sales_person" TEXT,
-ADD COLUMN     "sales_order_purposes" JSONB;
