d657ebb317734cf3ee5aed58852ce8c083ce3739
Phong Phan <fongfan999@gmail.com>
2025-11-05 23:58:03 +0700

    Revert "feat(misa): Restore #352 ( adding cash voucher mapping service)"

--- src/services/misa/index.js
+++ src/services/misa/index.js
@@ -1,11 +1,9 @@
 import VoucherMappingService from "services/misa/mapping/voucher-mapping-service";
-import CashVoucherMappingService from "services/misa/mapping/cash-voucher-mapping-service";
 import MisaWebhookHandler from "services/misa/webhook-handler";
 import MisaCallbackVoucherHandler from "services/misa/callback-voucher-handler";
 
 export default {
   VoucherMappingService,
-  CashVoucherMappingService,
   MisaWebhookHandler,
   MisaCallbackVoucherHandler
 };
