13f854dd9597161b5e7ce2ea1ef5f014c55c343b
BinDK <sarakenny123@yahoo.com>
2025-11-06 14:21:45 +0700

    feat(misa): added manual payment fetching service

--- src/services/payment/index.js
+++ src/services/payment/index.js
@@ -1,5 +1,9 @@
 import ManualPaymentService from "services/payment/manual-pay/payment";
+import QrPaymentFetchingService from "services/payment/qr_payment/fetch-service";
+import ManualPaymentFetchingService from "services/payment/manual-pay/fetch-service";
 
 export default {
-  ManualPaymentService: ManualPaymentService
+  ManualPaymentService: ManualPaymentService,
+  QrPaymentFetchingService,
+  ManualPaymentFetchingService
 };
