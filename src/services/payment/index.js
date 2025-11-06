fd316c42f0ddf1ce3070200a3cd72061ff5d60dc
BinDK <sarakenny123@yahoo.com>
2025-11-06 18:15:54 +0700

    feat(misa): added missing voucher sync in index

--- src/services/payment/index.js
+++ src/services/payment/index.js
@@ -1,9 +1,11 @@
 import ManualPaymentService from "services/payment/manual-pay/payment";
 import QrPaymentFetchingService from "services/payment/qr_payment/fetch-service";
 import ManualPaymentFetchingService from "services/payment/manual-pay/fetch-service";
+import MisaVoucherSyncService from "services/payment/misa/voucher-sync-service";
 
 export default {
   ManualPaymentService: ManualPaymentService,
   QrPaymentFetchingService,
-  ManualPaymentFetchingService
+  ManualPaymentFetchingService,
+  MisaVoucherSyncService
 };
