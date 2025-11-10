b03e31e9bafa42261ff879e7c3f1862f8542dc91
Phong Phan <fongfan999@gmail.com>
2025-11-10 20:43:56 +0700

    Update migration.sql

--- prisma/migrations/20251110133839_add_cloumn_on_workplace_diamonds/migration.sql
+++ prisma/migrations/20251110133839_add_cloumn_on_workplace_diamonds/migration.sql
@@ -5,4 +5,4 @@ ALTER TABLE "erpnext"."regions" ALTER COLUMN "region_name" SET DEFAULT '';
 ALTER TABLE "larksuite"."records" ALTER COLUMN "fields" SET DEFAULT '{}';
 
 -- AlterTable
-ALTER TABLE "workplace"."diamonds" ADD COLUMN     "final_discounted_price" DECIMAL;
+ALTER TABLE "workplace"."diamonds" ADD COLUMN "final_discounted_price" DECIMAL;
