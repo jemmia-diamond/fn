0926ece1e645c9ea16aa82e42c987093af1376fe
BinDK <sarakenny123@yahoo.com>
2025-11-14 01:21:02 +0700

    feat(misa): added select financial_status, order_processing_status for ref order

--- src/services/ecommerce/order-tracking/queries/get-initial-order.js
+++ src/services/ecommerce/order-tracking/queries/get-initial-order.js
@@ -3,24 +3,24 @@ import { Prisma } from "@prisma-cli";
 export async function getInitialOrder(db, orderId) {
   const orders = await db.$queryRaw`
     WITH RECURSIVE order_chain AS (
-        SELECT id, ref_order_id 
-        FROM haravan.orders 
+        SELECT id, ref_order_id
+        FROM haravan.orders
         WHERE id = ${orderId}
-        UNION ALL 
+        UNION ALL
         SELECT o.id, o.ref_order_id
         FROM haravan.orders o
             INNER JOIN order_chain oc ON o.id = oc.ref_order_id
     )
-    SELECT 
+    SELECT
     o.id ,
     o.order_number ,
-    o.created_at 
-    FROM haravan.orders o 
+    o.created_at
+    FROM haravan.orders o
     WHERE o.id IN (
         SELECT id FROM order_chain
     )
-    ORDER BY o.created_at ASC 
-    LIMIT 1 
+    ORDER BY o.created_at ASC
+    LIMIT 1
   `;
 
   return orders.length ? orders[0] : null;
@@ -29,18 +29,20 @@ export async function getInitialOrder(db, orderId) {
 export async function getRefOrderChain(db, orderId, includeSelf = false) {
   let sql = `
     WITH RECURSIVE order_chain AS (
-        SELECT id, ref_order_id, order_number, created_at
-        FROM haravan.orders 
+        SELECT id, ref_order_id, order_number, created_at, financial_status, order_processing_status
+        FROM haravan.orders
         WHERE id = ${orderId}
-        UNION ALL 
-        SELECT o.id, o.ref_order_id, o.order_number, o.created_at
+        UNION ALL
+        SELECT o.id, o.ref_order_id, o.order_number, o.created_at, o.financial_status, o.order_processing_status
         FROM haravan.orders o
             INNER JOIN order_chain oc ON o.id = oc.ref_order_id
     )
-    SELECT 
+    SELECT
         id,
         order_number,
-        created_at 
+        created_at,
+        financial_status,
+        order_processing_status
     FROM order_chain
   `;
 
