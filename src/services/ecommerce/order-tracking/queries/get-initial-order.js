781499908f3defc30ecd0fcd1fee64573aacf8c3
BinDK <sarakenny123@yahoo.com>
2025-11-14 09:26:41 +0700

    feat(misa): create function that get ref order with array of order id

--- src/services/ecommerce/order-tracking/queries/get-initial-order.js
+++ src/services/ecommerce/order-tracking/queries/get-initial-order.js
@@ -54,3 +54,47 @@ export async function getRefOrderChain(db, orderId, includeSelf = false) {
 
   return await db.$queryRaw`${Prisma.raw(sql)}`;
 }
+
+export async function getRefOrderChains(db, orderIds, includeSelf = false) {
+  if (!orderIds || orderIds.length === 0) {
+    return {};
+  }
+
+  const sql = `
+    WITH RECURSIVE order_chain AS (
+      SELECT id, ref_order_id, order_number, created_at, financial_status, order_processing_status, id AS root_id
+      FROM haravan.orders
+      WHERE id IN (${orderIds.join(",")})
+      UNION ALL
+      SELECT
+        o.id, o.ref_order_id, o.order_number, o.created_at, o.financial_status, o.order_processing_status,
+        oc.root_id
+      FROM haravan.orders o
+      INNER JOIN order_chain oc ON o.id = oc.ref_order_id
+    )
+    SELECT
+      root_id, id, order_number, created_at, financial_status, order_processing_status
+    FROM order_chain
+    ORDER BY root_id, created_at ASC
+  `;
+
+  const flatResults = await db.$queryRaw`${Prisma.raw(sql)}`;
+  const chains = flatResults.reduce((acc, order) => {
+    const { root_id, ...rest } = order;
+    if (!acc[root_id]) {
+      acc[root_id] = [];
+    }
+
+    if (includeSelf || root_id !== order.id) {
+      acc[root_id].push(rest);
+    }
+    return acc;
+  }, {});
+
+  for (const key in chains) {
+    if (chains[key].length === 0) {
+      delete chains[key];
+    }
+  }
+  return chains;
+}
