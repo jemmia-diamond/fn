a5dd014d2fc3f5ed6087b983c6cf12933329791a
NhatNam1607 <nhatnam160701@gmail.com>
2025-11-19 11:11:00 +0700

    feat(database): add default creted_at/updated_at

--- prisma/migrations/20251119041018_add_config_gold_pricing/migration.sql
+++ prisma/migrations/20251119041018_add_config_gold_pricing/migration.sql
@@ -10,7 +10,8 @@ ALTER TABLE "market_data"."gold_pricing" DROP COLUMN "type",
 ADD COLUMN     "gold_type" VARCHAR,
 ADD COLUMN     "location" VARCHAR,
 ADD COLUMN     "updated_at" TIMESTAMPTZ(6),
-ALTER COLUMN "time" SET DATA TYPE TIMESTAMPTZ(6);
+ALTER COLUMN "time" SET DATA TYPE TIMESTAMPTZ(6),
+ALTER COLUMN "created_at" SET DEFAULT CURRENT_TIMESTAMP;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "gold_pricing_location_gold_type_time_key" ON "market_data"."gold_pricing"("location", "gold_type", "time");
