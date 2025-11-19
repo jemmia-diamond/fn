bf2a769fe10a7f39aa5ac695ef833327f5dc5408
Phong Phan <fongfan999@gmail.com>
2025-11-20 01:00:57 +0700

    chore(db): remove prisma --no-engine flag

--- src/services/database.js
+++ src/services/database.js
@@ -6,8 +6,7 @@ import { PrismaPg } from "@prisma/adapter-pg";
 // const users = await db.$queryRaw`SELECT * FROM larksuite.users`;
 class Database {
   static createClient(env) {
-    // If DATABASE_URL is set, use it directly.
-    // Otherwise, use Hyperdrive.
+    // If DATABASE_URL is set, use it directly. Otherwise, use Hyperdrive.
     const connectionString = env.DATABASE_URL || env.HYPERDRIVE.connectionString;
     const adapter = new PrismaPg({ connectionString });
 
