f981644a87ab6366ee50b4023e10ba761263ac3a
BinDK <sarakenny123@yahoo.com>
2025-11-24 22:51:58 +0700

    feat(misa): refactor qr payment fetching service

--- src/services/payment/qr_payment/fetch-service.js
+++ src/services/payment/qr_payment/fetch-service.js
@@ -1,4 +1,3 @@
-import * as Sentry from "@sentry/cloudflare";
 import Database from "services/database";
 
 export default class QrPaymentFetchingService {
@@ -12,63 +11,63 @@ export default class QrPaymentFetchingService {
    * @returns qrPaymentTransaction[]
    */
   async byDateRangeAndNotSynced(startDate = null, endDate = null) {
-    try {
-      return await this.db.qrPaymentTransaction.findMany({
-        where: {
-          transfer_status: "success",
-          haravan_order: {
-            financial_status: { in: ["paid", "partially_paid"] }
-          },
-          misa_synced: false,
-          updated_at: {
-            gte: startDate,
-            lte: endDate
-          }
+    return await this.db.qrPaymentTransaction.findMany({
+      where: {
+        transfer_status: "success",
+        haravan_order: {
+          financial_status: { in: ["paid", "partially_paid"] }
         },
+        misa_synced: false,
+        updated_at: {
+          gte: startDate,
+          lte: endDate
+        }
+      },
+      select: this.misaRequiredFields()
+    });
+  }
+
+  misaRequiredFields() {
+    return {
+      id: true,
+      haravan_order_number: true,
+      transfer_amount: true,
+      bank_code: true,
+      bank_account_number: true,
+      customer_name: true,
+      transfer_note: true,
+      created_at: true,
+      updated_at: true,
+      haravan_order_id: true,
+      haravan_order: {
         select: {
           id: true,
-          haravan_order_number: true,
-          transfer_amount: true,
-          bank_code: true,
-          bank_account_number: true,
-          customer_name: true,
-          transfer_note: true,
-          created_at: true,
-          updated_at: true,
-          haravan_order_id: true,
-          haravan_order: {
+          name: true,
+          source: true,
+          gateway: true,
+          customer_id: true,
+          confirmed_at: true,
+          customer_default_address_address1: true,
+          customer_default_address_address2: true,
+          customer_default_address_ward: true,
+          customer_default_address_district: true,
+          customer_default_address_province: true,
+          customer_default_address_country: true,
+          ref_order_id: true,
+          user: {
             select: {
-              id: true,
-              name: true,
-              source: true,
-              gateway: true,
-              customer_id: true,
-              confirmed_at: true,
-              customer_default_address_address1: true,
-              customer_default_address_address2: true,
-              customer_default_address_ward: true,
-              customer_default_address_district: true,
-              customer_default_address_province: true,
-              customer_default_address_country: true,
-              ref_order_id: true,
-              user: {
+              first_name: true,
+              last_name: true,
+              misa_user: {
                 select: {
-                  first_name: true,
-                  last_name: true,
-                  misa_user: {
-                    select: {
-                      employee_code: true,
-                      email: true
-                    }
-                  }
+                  employee_code: true,
+                  email: true
                 }
               }
             }
           }
         }
-      });
-    } catch (error) {
-      Sentry.captureException(error);
-    }
+      }
+    };
   }
 }
