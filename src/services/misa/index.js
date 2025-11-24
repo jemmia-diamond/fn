14436a7a3fc765c4856383cfe23829860da2ba8c
BinDK <sarakenny123@yahoo.com>
2025-11-24 23:00:45 +0700

    feat(misa): update misa index

--- src/services/misa/index.js
+++ src/services/misa/index.js
@@ -4,6 +4,8 @@ import MisaCallbackVoucherHandler from "services/misa/callback-voucher-handler";
 import CashVoucherMappingService from "services/misa/mapping/cash-voucher-mapping-service";
 import InventoryItemSyncService from "services/misa/inventory-item/inventory-sync-service";
 import InventoryItemMappingService from "services/misa/mapping/inventory-item-mapping-service";
+import QrTransactionService from "services/misa/transactions/qr-transaction";
+import Utils from "services/misa/utils";
 
 export default {
   VoucherMappingService,
@@ -11,5 +13,7 @@ export default {
   MisaCallbackVoucherHandler,
   CashVoucherMappingService,
   InventoryItemSyncService,
-  InventoryItemMappingService
+  InventoryItemMappingService,
+  QrTransactionService,
+  Utils
 };
