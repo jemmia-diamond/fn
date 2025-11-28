c62a7b417d3ac1385ec7aca62877a128e1c89e1f
TungLam111 <lamtungphan8@gmail.com>
2025-11-28 17:24:19 +0700

    feat: update payload

--- src/services/haravan/collects/collect-service/collect-service.js
+++ src/services/haravan/collects/collect-service/collect-service.js
@@ -1,18 +1,46 @@
 import * as Sentry from "@sentry/cloudflare";
+import Database from "services/database";
 import HaravanAPI from "services/clients/haravan-client";
 import { BadRequestException } from "src/exception/exceptions";
 
 export default class CollectService {
   constructor(env) {
     this.env = env;
+    this.db = Database.instance(env);
   }
 
   async createCollect(body) {
-    const { haravan_collections_id, diamonds_id, products_id } = body.data.rows[0];
+    const { haravan_collection_id, diamond_id, product_id } = body.data.rows[0];
 
-    const targetProductId = diamonds_id || products_id;
+    const targetId = diamond_id || product_id;
 
-    if (!haravan_collections_id || !targetProductId) {
+    if (!haravan_collection_id || !targetId) {
+      return;
+    }
+
+    // Get Haravan Collection ID from ID
+    const collection = await this.db.$queryRaw`
+      SELECT haravan_id FROM workplace.haravan_collections WHERE id = ${haravan_collection_id} LIMIT 1
+    `;
+
+    if (!collection || collection.length === 0) {
+      return;
+    }
+    const realCollectionId = collection[0].haravan_id;
+
+    // Get Haravan Product ID from ID
+    let realProductId;
+    if (targetId) {
+      // TODO: Apply for jewelry
+      const diamond = await this.db.$queryRaw`
+        SELECT product_id FROM workplace.diamonds WHERE id = ${targetId} LIMIT 1
+      `;
+      if (diamond && diamond.length > 0) {
+        realProductId = diamond[0].product_id;
+      }
+    }
+
+    if (!realProductId || !realCollectionId) {
       return;
     }
 
@@ -25,19 +53,45 @@ export default class CollectService {
     const hrvClient = new HaravanAPI(HRV_API_KEY);
 
     const newCollect = await hrvClient.collect.createCollect({
-      "product_id": targetProductId,
-      "collection_id": haravan_collections_id
+      "product_id": realProductId,
+      "collection_id": realCollectionId
     });
 
     return newCollect;
   }
 
   async removeCollect(body) {
-    const { haravan_collections_id, diamonds_id, products_id } = body.data.rows[0];
+    const { haravan_collection_id, diamond_id, product_id } = body.data.rows[0];
 
-    const targetProductId = diamonds_id || products_id;
+    const targetId = diamond_id || product_id;
 
-    if (!haravan_collections_id || !targetProductId) {
+    if (!haravan_collection_id || !targetId) {
+      return;
+    }
+
+    // Get Haravan Collection ID
+    const collection = await this.db.$queryRaw`
+      SELECT haravan_id FROM workplace.haravan_collections WHERE id = ${haravan_collection_id} LIMIT 1
+    `;
+
+    if (!collection || collection.length === 0) {
+      return;
+    }
+    const realCollectionId = collection[0].haravan_id;
+
+    // Get Haravan Product ID
+    let realProductId;
+    if (targetId) {
+      // TODO: Apply for jewelry
+      const diamond = await this.db.$queryRaw`
+        SELECT product_id FROM workplace.diamonds WHERE id = ${targetId} LIMIT 1
+      `;
+      if (diamond && diamond.length > 0) {
+        realProductId = diamond[0].product_id;
+      }
+    }
+
+    if (!realProductId || !realCollectionId) {
       return;
     }
 
@@ -48,8 +102,8 @@ export default class CollectService {
     }
     const hrvClient = new HaravanAPI(HRV_API_KEY);
     const collectsData = await hrvClient.collect.getCollects({
-      "collection_id": haravan_collections_id,
-      "product_id": targetProductId
+      "collection_id": realCollectionId,
+      "product_id": realProductId
     });
 
     if (collectsData && collectsData.collects && collectsData.collects.length > 0) {
