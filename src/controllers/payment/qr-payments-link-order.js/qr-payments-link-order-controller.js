49050e8b17a00951d025f817948eb1bbd6635da1
TungLam111 <lamtungphan8@gmail.com>
2025-11-28 16:11:48 +0700

    feat: map deposit QR with real order

---
+++ src/controllers/payment/qr-payments-link-order.js/qr-payments-link-order-controller.js
@@ -0,0 +1,25 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc.js";
+import PaymentService from "services/payment";
+dayjs.extend(utc);
+
+export default class QRPaymentsLinkOrderController {
+
+  static async create(c) {
+    const { id } = c.req.param();
+    if (!id) {
+      return c.json({ error: "Missing 'id' parameter in URL" }, 400);
+    }
+    const body = await c.req.json();
+
+    if (!body || typeof body !== "object") {
+      return c.json({ error: "Invalid JSON body" }, 400);
+    }
+
+    const paymentService = new PaymentService.LinkQRWithRealOrderService(c.env);
+
+    await paymentService.mapTransaction(id, body);
+
+    return c.json({ success: true });
+  }
+}
