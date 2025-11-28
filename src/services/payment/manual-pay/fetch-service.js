2313bcee21324378e2a1a03b717f0956ecfe732f
BinDK <sarakenny123@yahoo.com>
2025-11-28 10:22:55 +0700

    refactor(payment): refactor query and remove try/catch block

--- src/services/payment/manual-pay/fetch-service.js
+++ src/services/payment/manual-pay/fetch-service.js
@@ -1,4 +1,3 @@
-import * as Sentry from "@sentry/cloudflare";
 import Database from "services/database";
 
 export default class ManualPaymentFetchingService {
@@ -24,71 +23,74 @@ export default class ManualPaymentFetchingService {
   }
 
   async _fetchManualPaymentsWithJoins(paymentType, startDate, endDate) {
-    try {
-      const whereCondition = {
-        transfer_status: "Xác nhận",
-        misa_synced: false,
-        created_date: {
-          gte: startDate,
-          lte: endDate
-        },
-        payment_type: paymentType === "cash" ? "Tiền Mặt" : { not: "Tiền Mặt" },
-        haravan_order: {
-          financial_status: {
-            in: ["paid", "partially_paid"]
-          }
+    const whereCondition = {
+      transfer_status: "Xác nhận",
+      misa_synced: false,
+      created_date: {
+        gte: startDate,
+        lte: endDate
+      },
+      payment_type: paymentType === "cash" ? "Tiền Mặt" : { not: "Tiền Mặt" },
+      haravan_order: {
+        financial_status: {
+          in: ["paid", "partially_paid"]
         }
-      };
+      }
+    };
 
-      return await this.db.manualPaymentTransaction.findMany({
-        where: whereCondition,
+    return await this.db.manualPaymentTransaction.findMany({
+      where: whereCondition,
+      select: this.misaRequiredFields()
+    });
+  }
+
+  misaRequiredFields() {
+    return {
+      uuid: true,
+      haravan_order_name: true,
+      transfer_amount: true,
+      payment_type: true,
+      branch: true,
+      bank_name: true,
+      bank_account: true,
+      transfer_note: true,
+      created_date: true,
+      receive_date: true,
+      updated_date: true,
+      haravan_order_id: true,
+      misa_sync_guid: true,
+      misa_synced_at: true,
+      haravan_order: {
         select: {
-          uuid: true,
-          haravan_order_name: true,
-          transfer_amount: true,
-          branch: true,
-          bank_name: true,
-          bank_account: true,
-          transfer_note: true,
-          created_date: true,
-          receive_date: true,
-          updated_date: true,
-          haravan_order_id: true,
-          haravan_order: {
+          id: true,
+          name: true,
+          source: true,
+          gateway: true,
+          customer_id: true,
+          confirmed_at: true,
+          customer_last_name: true,
+          customer_first_name: true,
+          customer_default_address_address1: true,
+          customer_default_address_address2: true,
+          customer_default_address_ward: true,
+          customer_default_address_district: true,
+          customer_default_address_province: true,
+          customer_default_address_country: true,
+          ref_order_id: true,
+          user: {
             select: {
-              id: true,
-              name: true,
-              source: true,
-              gateway: true,
-              customer_id: true,
-              confirmed_at: true,
-              customer_last_name: true,
-              customer_first_name: true,
-              customer_default_address_address1: true,
-              customer_default_address_address2: true,
-              customer_default_address_ward: true,
-              customer_default_address_district: true,
-              customer_default_address_province: true,
-              customer_default_address_country: true,
-              ref_order_id: true,
-              user: {
+              first_name: true,
+              last_name: true,
+              misa_user: {
                 select: {
-                  first_name: true,
-                  last_name: true,
-                  misa_user: {
-                    select: {
-                      employee_code: true,
-                      email: true
-                    }
-                  }
+                  employee_code: true,
+                  email: true
                 }
               }
             }
           }
         }
-      });
-    } catch (error) {
-      Sentry.captureException(error);
-    }
+      }
+    };
   }
 }
