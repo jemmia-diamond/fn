597786bb30b97b022c8ccc11f633b7697d150a36
TungLam111 <lamtungphan8@gmail.com>
2025-11-28 12:50:33 +0700

    feat: migrate map to sepay

--- src/controllers/payment/index.js
+++ src/controllers/payment/index.js
@@ -1,7 +1,9 @@
 import ManualPaymentsController from "controllers/payment/manual-pay/manual-payment-controller";
 import QRPaymentsController from "controllers/payment/qr-pay/qr-payments-controller";
+import QRPaymentsMapSepayController from "controllers/payment/qr-payments-map-sepay/qr-payments-map-sepay-controller";
 
 export default {
   ManualPaymentsController,
-  QRPaymentsController
+  QRPaymentsController,
+  QRPaymentsMapSepayController
 };
