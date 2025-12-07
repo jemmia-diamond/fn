a41c4a6b98ebfb59f7a4aa42bd281bdfce66e282
BinDK <sarakenny123@yahoo.com>
2025-12-07 18:41:49 +0700

    feat(erpnext): added accounting constant

--- src/services/erp/accounting/index.js
+++ src/services/erp/accounting/index.js
@@ -1,7 +1,9 @@
 import PaymentEntryService from "services/erp/accounting/payment-entry/payment-entry";
 import SepayTransactionService from "services/erp/accounting/sepay-transaction/sepay-transaction";
+import * as Constants from "services/erp/accounting/constants";
 
 export default {
-  PaymentEntryService: PaymentEntryService,
-  SepayTransactionService: SepayTransactionService
+  PaymentEntryService,
+  SepayTransactionService,
+  Constants
 };
