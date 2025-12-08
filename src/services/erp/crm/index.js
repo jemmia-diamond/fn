e0937c8618585e013bd7718408b53462b52fa3c6
NhatNam1607 <nhatnam160701@gmail.com>
2025-12-08 14:00:49 +0700

    feat(sync): sync lead_products form erp to database

--- src/services/erp/crm/index.js
+++ src/services/erp/crm/index.js
@@ -1,6 +1,7 @@
 import LeadService from "services/erp/crm/lead/lead";
 import LeadDemandService from "services/erp/crm/lead_demand/lead_demand";
 import LeadBudgetService from "services/erp/crm/lead_budget/lead_budget";
+import LeadProductService from "services/erp/crm/lead_product/lead_product";
 import RegionService from "services/erp/crm/region/region";
 import ProvinceService from "services/erp/crm/province/province";
 
@@ -8,6 +9,7 @@ export default {
   LeadService: LeadService,
   LeadDemandService: LeadDemandService,
   LeadBudgetService: LeadBudgetService,
+  LeadProductService: LeadProductService,
   RegionService: RegionService,
   ProvinceService: ProvinceService
 };
