a2e4bd6ce217737228743c552984fbdb4bbb287d
BinDK <sarakenny123@yahoo.com>
2025-12-10 15:10:00 +0700

    chore(erpnext): mising conflict resolve

--- src/services/erp/accounting/index.js
+++ src/services/erp/accounting/index.js
@@ -1,6 +1,7 @@
 import PaymentEntryService from "services/erp/accounting/payment-entry/payment-entry";
 import SepayTransactionService from "services/erp/accounting/sepay-transaction/sepay-transaction";
 import * as Constants from "services/erp/accounting/constants";
+import BankTransactionVerificationService from "services/erp/accounting/payment-entry/verification-service";
 
 export default {
   PaymentEntryService: PaymentEntryService,
