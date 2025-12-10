ac60e26a38a292e9027cd905536f92e5d5b1694d
BinDK <sarakenny123@yahoo.com>
2025-12-10 18:37:45 +0700

    chore(erpnext): using correct queue

--- src/controllers/webhook/frappe/erp/customer/customer.js
+++ src/controllers/webhook/frappe/erp/customer/customer.js
@@ -2,7 +2,7 @@ export default class FrappeERPCustomerController {
   static async create(ctx) {
     const data = await ctx.req.json();
     const erpTopic = ctx.req.header("erp-topic") || "";
-    await ctx.env.ERPNEXT_CUSTOMER_QUEUE.send({ data, erpTopic });
+    await ctx.env.ERPNEXT_SELLING_QUEUE.send({ data, erpTopic });
     return ctx.json({ message: "Customer webhook received", status: 200 });
   }
 }
