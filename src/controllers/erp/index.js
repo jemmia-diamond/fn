6478250a073e0b718d9647ce14f0a3b5834578f4
BinDK <sarakenny123@yahoo.com>
2025-12-15 00:51:42 +0700

    feat(erp): registered erp customer controller

--- src/controllers/erp/index.js
+++ src/controllers/erp/index.js
@@ -1,9 +1,11 @@
 import SalesOrderController from "controllers/erp/sales-order-controller";
 import LeadController from "controllers/erp/lead-controller";
 import SalesOrderNotificationController from "controllers/erp/sales-order-notification-controller";
+import CustomersController from "controllers/erp/customers-controller";
 
 export default {
   SalesOrderController,
   LeadController,
-  SalesOrderNotificationController
+  SalesOrderNotificationController,
+  CustomersController
 };
