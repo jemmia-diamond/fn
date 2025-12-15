a91045b964303c2a8f8c0025aafaaf04434a2743
TungLam111 <lamtungphan8@gmail.com>
2025-12-15 10:30:59 +0700

    feat: add ZaloPay transaction fields to the `sepay_transaction` model and persist webhook data

---
+++ prisma/migrations/20251215032821_add_zalopay_fields/migration.sql
@@ -0,0 +1,14 @@
+-- AlterTable
+ALTER TABLE "payment"."sepay_transaction" ADD COLUMN     "amount" BIGINT,
+ADD COLUMN     "app_id" INTEGER,
+ADD COLUMN     "app_time" BIGINT,
+ADD COLUMN     "app_trans_id" VARCHAR(255),
+ADD COLUMN     "app_user" VARCHAR(255),
+ADD COLUMN     "channel" INTEGER,
+ADD COLUMN     "discount_amount" BIGINT,
+ADD COLUMN     "embed_data" JSONB,
+ADD COLUMN     "item" JSONB,
+ADD COLUMN     "merchant_user_id" VARCHAR(255),
+ADD COLUMN     "server_time" BIGINT,
+ADD COLUMN     "user_fee_amount" BIGINT,
+ADD COLUMN     "zp_trans_id" BIGINT;
