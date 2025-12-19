76c29861b7c409070791e8326418f572f06195aa
TungLam111 <lamtungphan8@gmail.com>
2025-12-19 10:07:32 +0700

    feat: revert test code

--- src/controllers/webhook/frappe/index.js
+++ src/controllers/webhook/frappe/index.js
@@ -1,3 +1,4 @@
+import { verifyHmacBase64Auth } from "auth/hmac-base64-auth";
 import FrappePancakeConversationAssignmentController from "controllers/webhook/frappe/pancake/conversation-assignment";
 import FrappeERPContactsController from "controllers/webhook/frappe/erp/contacts/contacts";
 import FrappeERPSalesOrderController from "controllers/webhook/frappe/erp/sales-order/sales-order";
@@ -7,7 +8,7 @@ import FrappeERPCustomerController from "controllers/webhook/frappe/erp/customer
 export default class FrappeWebhook {
   static register(webhook) {
     const frappeWebhookNamespace = webhook.basePath("/frappe");
-    // frappeWebhookNamespace.use("*", verifyHmacBase64Auth("X-Frappe-Webhook-Signature", "FRAPPE_WEBHOOK_SECRET"));
+    frappeWebhookNamespace.use("*", verifyHmacBase64Auth("X-Frappe-Webhook-Signature", "FRAPPE_WEBHOOK_SECRET"));
     frappeWebhookNamespace.post("/pancake/conversation_assignments", FrappePancakeConversationAssignmentController.create);
     frappeWebhookNamespace.post("/erp/contacts", FrappeERPContactsController.create);
     frappeWebhookNamespace.post("/erp/sales-orders", FrappeERPSalesOrderController.create);
