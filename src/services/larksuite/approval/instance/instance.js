4db8c815b8dd39d0978580e47bcae6a1a9a1460e
BinDK <sarakenny123@yahoo.com>
2026-01-07 17:37:04 +0700

    feat(approval): applied ORM

--- src/services/larksuite/approval/instance/instance.js
+++ src/services/larksuite/approval/instance/instance.js
@@ -56,48 +56,39 @@ export default class InstanceService {
       }
     }
 
-    for (const transformedInstance of transformedInstances) {
-      await db.$executeRaw`
-        INSERT INTO larksuite.instances (
-          instance_code,
-          approval_code,
-          approval_name,
-          status,
-          form,
-          start_time,
-          end_time,
-          serial_number,
-          user_id,
-          uuid,
-          department_id,
-          form_data
-        ) VALUES (
-          ${transformedInstance.instance_code},
-          ${transformedInstance.approval_code},
-          ${transformedInstance.approval_name},
-          ${transformedInstance.status},
-          ${transformedInstance.form},
-          ${transformedInstance.start_time},
-          ${transformedInstance.end_time},
-          ${transformedInstance.serial_number},
-          ${transformedInstance.user_id},
-          ${transformedInstance.uuid},
-          ${transformedInstance.department_id},
-          ${transformedInstance.form_data}
-        )
-        ON CONFLICT (instance_code) DO UPDATE SET
-          approval_code = EXCLUDED.approval_code,
-          approval_name = EXCLUDED.approval_name,
-          status = EXCLUDED.status,
-          form = EXCLUDED.form,
-          start_time = EXCLUDED.start_time,
-          end_time = EXCLUDED.end_time,
-          serial_number = EXCLUDED.serial_number,
-          user_id = EXCLUDED.user_id,
-          uuid = EXCLUDED.uuid,
-          department_id = EXCLUDED.department_id,
-          form_data = EXCLUDED.form_data;
-      `;
+    for (const instance of transformedInstances) {
+      await db.larksuitInstances.upsert({
+        where: {
+          instance_code: instance.instance_code
+        },
+        update: {
+          approval_code: instance.approval_code,
+          approval_name: instance.approval_name,
+          status: instance.status,
+          form: instance.form,
+          start_time: new Date(instance.start_time),
+          end_time: new Date(instance.end_time),
+          serial_number: instance.serial_number,
+          user_id: instance.user_id,
+          uuid: instance.uuid,
+          department_id: instance.department_id,
+          form_data: instance.form_data
+        },
+        create: {
+          instance_code: instance.instance_code,
+          approval_code: instance.approval_code,
+          approval_name: instance.approval_name,
+          status: instance.status,
+          form: instance.form,
+          start_time: new Date(instance.start_time),
+          end_time: new Date(instance.end_time),
+          serial_number: instance.serial_number,
+          user_id: instance.user_id,
+          uuid: instance.uuid,
+          department_id: instance.department_id,
+          form_data: instance.form_data
+        }
+      });
     }
   }
 
