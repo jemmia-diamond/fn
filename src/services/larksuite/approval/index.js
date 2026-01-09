427d093decf273b04f18c0f4f740114a5e8a9266
BinDK <sarakenny123@yahoo.com>
2026-01-09 14:29:32 +0700

    feat(larksuite): added separate buyback instance service

--- src/services/larksuite/approval/index.js
+++ src/services/larksuite/approval/index.js
@@ -1,5 +1,7 @@
 import InstanceService from "services/larksuite/approval/instance/instance";
+import BuyBackInstanceService from "services/larksuite/approval/instance/buyback-instance";
 
 export default {
-  InstanceService: InstanceService
+  InstanceService: InstanceService,
+  BuyBackInstanceService
 };
