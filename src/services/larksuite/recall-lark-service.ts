b0851f0fa46165462f1020b930a41fac04a5a461
Nhats 💎 <nhatsdevil@gmail.com>
2026-02-02 17:55:33 +0700

    feat(post-recall): update recall for image

--- src/services/larksuite/recall-lark-service.ts
+++ src/services/larksuite/recall-lark-service.ts
@@ -229,7 +229,11 @@ export default class RecallLarkService {
     const iv = Buffer.from(encrypted, "base64").subarray(0, 16);
     const encryptedData = Buffer.from(encrypted, "base64").subarray(16);
 
-    const decipher = crypto.createDecipheriv("aes-256-cbc", key as any, iv as any);
+    const decipher = crypto.createDecipheriv(
+      "aes-256-cbc",
+      key as any,
+      iv as any
+    );
     let decrypted = decipher.update(encryptedData as any);
     decrypted = Buffer.concat([decrypted, decipher.final()] as any);
 
