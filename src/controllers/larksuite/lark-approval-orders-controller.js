394b4c8205a056ece03054bcd6e7f62632b4a3f2
TungLam111 <lamtungphan8@gmail.com>
2026-02-04 18:27:52 +0700

    feat: get order by linked params

--- src/controllers/larksuite/lark-approval-orders-controller.js
+++ src/controllers/larksuite/lark-approval-orders-controller.js
@@ -1,4 +1,4 @@
-import FrappeClient from "src/frappe/frappe-client";
+import ApprovalOrderService from "services/larksuite/approval/approval-order-service";
 
 export default class LarkApprovalOrdersController {
   static async create(ctx) {
@@ -14,55 +14,8 @@ export default class LarkApprovalOrdersController {
         });
       }
 
-      const frappeClient = new FrappeClient({
-        url: ctx.env.JEMMIA_ERP_BASE_URL,
-        apiKey: ctx.env.JEMMIA_ERP_API_KEY,
-        apiSecret: ctx.env.JEMMIA_ERP_API_SECRET
-      });
-
-      const filters = [["docstatus", "!=", 2]];
-
-      if (linkage_params && linkage_params.phone) {
-        filters.push(["contact_phone", "like", `%${linkage_params.phone}%`]);
-      }
-
-      const orders = await frappeClient.getList("Sales Order", {
-        fields: ["name", "customer", "grand_total", "currency", "transaction_date"],
-        filters: filters,
-        order_by: "creation desc",
-        limit_page_length: 50
-      });
-
-      const options = orders.map(order => {
-        const id = order.name;
-        const value = id;
-        return {
-          id,
-          value,
-          isDefault: false
-        };
-      });
-
-      const texts = {};
-      orders.forEach(order => {
-        const key = order.name;
-        const formattedTotal = new Intl.NumberFormat("vi-VN", { style: "currency", currency: order.currency || "VND" }).format(order.grand_total);
-        const label = `${order.name} - ${order.customer} - ${formattedTotal} (${order.transaction_date})`;
-        texts[key] = label;
-      });
-
-      const i18nResources = [
-        {
-          locale: "en_us",
-          isDefault: true,
-          texts
-        },
-        {
-          locale: "vi_vn",
-          isDefault: false,
-          texts
-        }
-      ];
+      const service = new ApprovalOrderService(ctx.env);
+      const { options, i18nResources } = await service.searchOrders(linkage_params);
 
       return ctx.json({
         code: 0,
