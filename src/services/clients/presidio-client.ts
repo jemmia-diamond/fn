eaee09d4413895c73e73850b59aa59f5c0a1f470
Nhats 💎 <nhatsdevil@gmail.com>
2026-02-09 16:12:27 +0700

    chore(remove localhost url): remove localhost baseurl

--- src/services/clients/presidio-client.ts
+++ src/services/clients/presidio-client.ts
@@ -1,8 +1,7 @@
 import { createAxiosClient } from "services/utils/http-client";
 import { Jimp } from "jimp";
 
-// const BASE_URL = "https://presidio.jemmia.vn";
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://presidio.jemmia.vn";
 
 export interface AnalyzeImageResponse {
   has_handwriting: boolean;
