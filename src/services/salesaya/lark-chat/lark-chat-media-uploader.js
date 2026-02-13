997cadb1685c3d5d6ca5701b4f6a9379303364b8
quanghung0808 <duongquanghung0122@gmail.com>
2026-02-13 12:40:19 +0700

    refactor: initialize the HTTP client as an instance property

--- src/services/salesaya/lark-chat/lark-chat-media-uploader.js
+++ src/services/salesaya/lark-chat/lark-chat-media-uploader.js
@@ -1,17 +1,16 @@
 import { createAxiosClient } from "services/utils/http-client";
 
-const httpClient = createAxiosClient();
-
 export default class LarkChatMediaUploader {
   constructor(uploadUrl) {
     this.uploadUrl = uploadUrl;
+    this.client = createAxiosClient();
   }
 
   async upload(buffer, filename) {
     const form = new FormData();
     form.append("file", new Blob([buffer]), filename);
 
-    const { data } = await httpClient.post(this.uploadUrl, form);
+    const { data } = await this.client.post(this.uploadUrl, form);
     return data?.url || data?.link || null;
   }
 }
