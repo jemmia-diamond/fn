1cc8cdaabf5f2573bb7a121e443a0da5d7ff0fd4
BinDK <sarakenny123@yahoo.com>
2026-03-05 13:27:11 +0700

    chore(haravan): migrate collection_product from lambda project

--- src/services/ecommerce/index.js
+++ src/services/ecommerce/index.js
@@ -10,6 +10,7 @@ import DiamondService from "services/ecommerce/diamond/diamond";
 import FormService from "services/ecommerce/form/form";
 import DiamondCollectService from "services/ecommerce/diamond/diamond-collect-service";
 import VariantSyncService from "services/ecommerce/variant/variant-sync-service";
+import CollectionProductSyncService from "services/ecommerce/collection-product/collection-product-sync-service";
 
 export default {
   ProductService: ProductService,
@@ -23,5 +24,6 @@ export default {
   JewelryDiamondPairService: JewelryDiamondPairService,
   FormService: FormService,
   DiamondCollectService: DiamondCollectService,
-  VariantSyncService
+  VariantSyncService,
+  CollectionProductSyncService
 };
