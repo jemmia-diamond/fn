3e4cef31618e9f2f40fea0b30af93de8d12ba00e
BinDK <sarakenny123@yahoo.com>
2026-03-10 18:21:35 +0700

    feat(google-ads): add marketing google ads group table

---
+++ prisma/migrations/20260310110444_add_google_ads_adgroup/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "marketing"."google_ads_adgroups" (
+    "uuid" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "id" TEXT NOT NULL,
+    "campaign_id" TEXT NOT NULL,
+    "campaign_name" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "status" TEXT,
+    "type" TEXT,
+    "synced_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "google_ads_adgroups_pkey" PRIMARY KEY ("uuid")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "google_ads_adgroups_id_key" ON "marketing"."google_ads_adgroups"("id");
