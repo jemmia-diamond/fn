c2cc1e15bbaed36ec858d69c5b5b631202b0c41f
Nhats 💎 <nhatsdevil@gmail.com>
2026-03-16 11:43:01 +0700

    chore(remove phone helper): remove phone helper utils

--- src/services/utils/phone-helper.ts
+++
@@ -1,8 +0,0 @@
-export default class PhoneHelper {
-  private static readonly ORIGINAL_PHONE_REGEX =
-    /(?:\+?84|0)(?:3|5|7|8|9)[0-9]{8}\b/g;
-
-  static extractPhones(text: string): string[] {
-    return text.match(this.ORIGINAL_PHONE_REGEX) ?? [];
-  }
-}
