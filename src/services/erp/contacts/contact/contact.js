7196c3cc0e2962a3f4c88d32ec0ccc5c138c6c83
tunglamphandam <lamtungphan8@gmail.com>
2026-04-03 09:26:12 +0700

    fix: normalize website phone contact

--- src/services/erp/contacts/contact/contact.js
+++ src/services/erp/contacts/contact/contact.js
@@ -5,6 +5,7 @@ import utc from "dayjs/plugin/utc.js";
 import Database from "services/database";
 import { fetchContactsFromERP, saveContactsToDatabase, deleteContactFromDatabase } from "services/erp/contacts/contact/utils/contact-helppers";
 import { parseURLParameters, getParam } from "services/utils/url-helper";
+import { normalizeToStandardFormat } from "services/utils/phone-utils";
 
 dayjs.extend(utc);
 
@@ -89,6 +90,8 @@ export default class ContactService {
     const conversionUrlParams = parseURLParameters(data.raw_data.conversion_url);
     const originalUrlPageParams = parseURLParameters(data.raw_data.origin_url_page);
 
+    const normalizedPhone = normalizeToStandardFormat(data.raw_data.phone);
+
     const contactData = {
       doctype: this.doctype,
       custom_uuid: data.custom_uuid,
@@ -96,7 +99,7 @@ export default class ContactService {
       inserted_at: dayjs(data.database_created_at).utc().format("YYYY-MM-DD HH:mm:ss"),
       phone_nos: [
         {
-          "phone": data.raw_data.phone,
+          "phone": normalizedPhone,
           "is_primary_phone": 1
         }
       ],
@@ -139,7 +142,7 @@ export default class ContactService {
     const defaultContact = await this.frappeClient.getList(this.doctype, {
       filters: [
         ["Dynamic Link", "link_name", "=", lead.name],
-        ["Contact Phone", "phone", "=", data.raw_data.phone],
+        ["Contact Phone", "phone", "=", normalizedPhone],
         ["source", "=", lead.source],
         ["custom_uuid", "=", null]
       ]
