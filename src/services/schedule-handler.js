1777d7909028c0fcd55eb2c1feb1ea075ddc2f48
BinDK <sarakenny123@yahoo.com>
2026-04-07 17:28:21 +0700

    chore(haravan): move the haravan warehouse inventory back to 5 minute cron

--- src/services/schedule-handler.js
+++ src/services/schedule-handler.js
@@ -39,6 +39,7 @@ export default {
       await new Misa.InventoryItemSyncService(env).syncInventoryItems();
       await new Haravan.OrderModule.DatabaseSyncService(env).sync();
       await new Haravan.Product.DatabaseSyncService(env).sync();
+      await new Haravan.WarehouseInventory.DatabaseSyncService(env).sync();
       break;
     case "*/10 * * * *": // At every 10th minute
       await ERP.Selling.SerialService.syncSerialsToERP(env);
@@ -50,7 +51,6 @@ export default {
       break;
     case "*/15 * * * *": // At every 15th minute
       await ERP.Contacts.ContactService.cronSyncContactsToDatabase(env);
-      await new Haravan.WarehouseInventory.DatabaseSyncService(env).sync();
       break;
     case "*/20 * * * *": // At every 20th minute
       await ERP.Selling.SalesOrderService.cronSyncSalesOrdersToDatabase(env);
