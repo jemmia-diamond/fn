480bb32d7abdaf3296801d94a6e5537f675db389
BinDK <sarakenny123@yahoo.com>
2026-04-08 15:23:29 +0700

    chore: sync accounting sales order materialled view table every thirty min

--- src/services/schedule-handler.js
+++ src/services/schedule-handler.js
@@ -60,6 +60,7 @@ export default {
       await ERP.Contacts.AddressService.cronSyncAddressesToDatabase(env);
       await Ecommerce.ProductService.refreshMaterializedViews(env);
       await DatabaseOperations.MaterializedViewService.refresh30Minutes(env);
+      await new Haravan.AccountingSalesOrders.LarkSyncService(env).sync();
       break;
     case "0 */3 * * *": // At every 3rd hour
       await InventoryCMS.InventoryCheckSheetService.syncInventoryCheckSheetToDatabase(env);
