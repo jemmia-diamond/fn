ae63d934ce747c5e61ff671455e8458b962bdbda
BinDK <sarakenny123@yahoo.com>
2026-04-15 13:13:55 +0700

    chore(inventory-notification): rm try-catch block in controller

--- src/controllers/webhook/inventory-cms/inventory-check-notification.js
+++ src/controllers/webhook/inventory-cms/inventory-check-notification.js
@@ -1,16 +1,9 @@
 import InventoryCms from "services/inventory-cms";
-import * as Sentry from "@sentry/cloudflare";
 
 export default class InventoryCheckNotificationController {
   static async create(ctx) {
     const data = await ctx.req.json();
-    try {
-      const result = await InventoryCms.CheckSheetNotificationService.processInventoryCheck(data?.payload, ctx.env);
-      return ctx.json(result);
-    } catch (error) {
-      console.warn(`InventoryCheckNotification failed at ${new Date()}`);
-      console.warn(error);
-      Sentry.captureException(error);
-    }
+    const result = await InventoryCms.CheckSheetNotificationService.processInventoryCheck(data?.payload, ctx.env);
+    return ctx.json(result);
   }
 }
