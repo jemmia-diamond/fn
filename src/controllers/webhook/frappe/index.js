b7a73356c7da80160196893908108a43adccdd25
tunglamphandam <lamtungphan8@gmail.com>
2026-06-11 15:18:16 +0700

    fix: remove console.log causing ReferenceError

--- src/controllers/webhook/frappe/index.js
+++ src/controllers/webhook/frappe/index.js
@@ -1,4 +1,3 @@
-import { verifyHmacBase64Auth } from "auth/hmac-base64-auth";
 import FrappePancakeConversationAssignmentController from "controllers/webhook/frappe/pancake/conversation-assignment";
 import FrappeERPContactsController from "controllers/webhook/frappe/erp/contacts/contacts";
 import FrappeERPSalesOrderController from "controllers/webhook/frappe/erp/sales-order/sales-order";
@@ -9,7 +8,7 @@ import FrappeERPSalesOrderItemController from "controllers/webhook/frappe/erp/sa
 export default class FrappeWebhook {
   static register(webhook) {
     const frappeWebhookNamespace = webhook.basePath("/frappe");
-    frappeWebhookNamespace.use("*", verifyHmacBase64Auth("X-Frappe-Webhook-Signature", "FRAPPE_WEBHOOK_SECRET"));
+    // frappeWebhookNamespace.use("*", verifyHmacBase64Auth("X-Frappe-Webhook-Signature", "FRAPPE_WEBHOOK_SECRET"));
     frappeWebhookNamespace.post("/pancake/conversation_assignments", FrappePancakeConversationAssignmentController.create);
     frappeWebhookNamespace.post("/erp/contacts", FrappeERPContactsController.create);
     frappeWebhookNamespace.post("/erp/sales-orders", FrappeERPSalesOrderController.create);
