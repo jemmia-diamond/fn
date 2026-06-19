398a0c2c50e3fea28d62e7b963f866d808c034ea
tunglamphandam <lamtungphan8@gmail.com>
2026-06-19 16:37:59 +0700

    chore: resolve ts error in prisma.config.ts

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -1,11 +1,8 @@
-// @ts-expect-error ignore missing prisma config type
 import { defineConfig } from "@prisma/config";
 
 export default defineConfig({
-  earlyAccess: true,
   schema: "./schema.prisma",
   datasource: {
-    // @ts-expect-error ignore missing process type
     url: process.env.DATABASE_URL
   }
 });
