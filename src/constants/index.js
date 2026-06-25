7164e18695d7e08984a0877be9b63615f09bd150
tunglamphandam <lamtungphan8@gmail.com>
2026-06-25 18:17:00 +0700

    chore: update to using env

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,4 @@
 export const TIMEZONE_VIETNAM = "Asia/Ho_Chi_Minh";
 export const MISSING_SERIAL_START_DATE = "2026-05-01T00:00:00";
+export const DISABLE_FEATURE_FLAG = "-1";
+export const ENABLE_FEATURE_FLAG = "1";
