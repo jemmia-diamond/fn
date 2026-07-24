378acc678390a8eb7d8a7e0907eee5ac2b7a854a
NHΛT TRΛN <contact@nhatt.com>
2026-07-24 10:52:34 +0700

    chore: remove try catch scope

--- src/controllers/webhook/nocodb/design-images-retouch-to-haravan.ts
+++ src/controllers/webhook/nocodb/design-images-retouch-to-haravan.ts
@@ -5,12 +5,8 @@ export default class DesignImagesRetouchToHaravanController {
   static async handle(ctx: Context) {
     const payload = await ctx.req.json();
 
-    try {
-      const service = new RetouchToHaravanService(ctx.env as any);
-      await service.sync(payload);
-      return ctx.json({ message: "OK" }, { status: 200 });
-    } catch (err: any) {
-      return ctx.json({ message: err.message }, { status: 400 });
-    }
+    const service = new RetouchToHaravanService(ctx.env as any);
+    await service.sync(payload);
+    return ctx.json({ message: "OK" }, { status: 200 });
   }
 }
--- src/controllers/webhook/nocodb/design-images-upload-retouch-to-nocodb.ts
+++ src/controllers/webhook/nocodb/design-images-upload-retouch-to-nocodb.ts
@@ -5,12 +5,8 @@ export default class DesignImagesUploadRetouchToNocoDBController {
   static async handle(ctx: Context) {
     const payload = await ctx.req.json();
 
-    try {
-      const service = new RetouchUploaderService(ctx.env as any);
-      await service.sync(payload);
-      return ctx.json({ message: "OK" }, { status: 200 });
-    } catch (err: any) {
-      return ctx.json({ message: err.message }, { status: 400 });
-    }
+    const service = new RetouchUploaderService(ctx.env as any);
+    await service.sync(payload);
+    return ctx.json({ message: "OK" }, { status: 200 });
   }
 }
