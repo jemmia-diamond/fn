844ecabc0d77472b77479a118c48802780eee8b0
tunglamphandam <lamtungphan8@gmail.com>
2026-08-21 17:16:33 +0700

    feat: implement NocoDB webhook endpoint for automatic product creation on Haravan

--- src/controllers/webhook/nocodb/index.js
+++ src/controllers/webhook/nocodb/index.js
@@ -1,7 +1,8 @@
 
 import CollectController from "controllers/webhook/nocodb/collect";
-import SetsController from "controllers/webhook/nocodb/sets";
 import DiamondsController from "controllers/webhook/nocodb/diamonds";
+import ProductsController from "controllers/webhook/nocodb/products";
+import SetsController from "controllers/webhook/nocodb/sets";
 
 export default class NocoWebhook {
   static async register(webhook) {
@@ -9,5 +10,6 @@ export default class NocoWebhook {
     nocoWebhookNamespace.post("collects", CollectController.create);
     nocoWebhookNamespace.post("sets", SetsController.handle);
     nocoWebhookNamespace.post("diamonds", DiamondsController.handle);
+    nocoWebhookNamespace.post("products", ProductsController.handle);
   }
 }
