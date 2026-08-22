a60264a095db198487869919ac1575d68645d5de
tunglamphandam <lamtungphan8@gmail.com>
2026-08-22 18:57:37 +0700

    chore: migrate haravan collections creator and updater from lambda

--- src/controllers/webhook/nocodb/index.js
+++ src/controllers/webhook/nocodb/index.js
@@ -1,9 +1,9 @@
-
 import { verifyNocoWebhook } from "auth/nocodb-auth";
 import CollectController from "controllers/webhook/nocodb/collect";
 import DiamondsController from "controllers/webhook/nocodb/diamonds";
 import ProductsController from "controllers/webhook/nocodb/products";
 import SetsController from "controllers/webhook/nocodb/sets";
+import HaravanCollectionsController from "controllers/webhook/nocodb/haravan-collections";
 
 export default class NocoWebhook {
   static async register(webhook) {
@@ -13,5 +13,6 @@ export default class NocoWebhook {
     nocoWebhookNamespace.post("sets", SetsController.handle);
     nocoWebhookNamespace.post("diamonds", DiamondsController.handle);
     nocoWebhookNamespace.post("products", ProductsController.handle);
+    nocoWebhookNamespace.post("haravan-collections", HaravanCollectionsController.handle);
   }
 }
