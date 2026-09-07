2966f9caad28b1cd7c85afc559aa0c46aee92d21
BinDK <sarakenny123@yahoo.com>
2026-09-07 14:20:33 +0700

    chore(lead): filling the gap of lead creation via call log

--- src/services/erp/crm/lead/lead.js
+++ src/services/erp/crm/lead/lead.js
@@ -342,7 +342,7 @@ export default class LeadService {
 
   static async syncCallLogLead(env) {
     const leadService = new LeadService(env);
-    const timeThreshold = dayjs().utc().subtract(3, "hours").subtract(5, "minutes").format("YYYY-MM-DD HH:mm:ss");
+    const timeThreshold = dayjs().utc().subtract(10, "minutes").format("YYYY-MM-DD HH:mm:ss");
     const callLogs = await leadService.frappeClient.getList("Call Log", {
       filters: [
         ["creation", ">=", timeThreshold],
